609002a8d77ee595f599617762801d1ba0be3e49
prachipundir29 <prachipundir64@gmail.com>
2025-05-19 08:22:49 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33afsgdsg"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+//   console.log(typeof valueInNumber);
+//   console.log( valueInNumber);
+
+
+// When we try to convert string, boolean, null, undefined into number-
+//"33"=> 33
+// "33abs" => NaN
+//true=> 1; false=> 0
+
+let isLoggedIn = "Prachi Pundir"
+let booleanIsLoggedIn = Boolean(isLoggedIn) 
+  console.log(booleanIsLoggedIn);
+
+  // 1=> true; 0=> false
+  //"" => false
+  //"Prachi Pundir" => true
+
+
+  let someNumber = 44
+  let stringNumber = String(someNumber)
+    console.log( typeof stringNumber );
