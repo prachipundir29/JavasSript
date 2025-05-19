58fd2f215e2e4e0c214d11511c3c4bae927be905
prachipundir29 <prachipundir64@gmail.com>
2025-05-19 17:26:09 +0000

    summary of datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+// There are teo types of datatype
+//1. Primitive     2. Non-Primitive
+
+
+// #Primitive
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+ 
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+
+console.log(id === anotherId );
+
+const bigNumber = 24357865243124375n   //BigInt
+
+// #Reference (Non Primitive)
+
+// Array, Objects, Functions
