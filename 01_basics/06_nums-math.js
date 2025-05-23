e58e1861ef79231a0c80ce2275ed08e0acfb6fd0
prachipundir29 <prachipundir64@gmail.com>
2025-05-23 10:10:45 +0000

    Math and Number

---
+++ 01_basics/06_nums-math.js
@@ -0,0 +1,36 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(200)
+// console.log( balance);
+
+// console.log( balance.toString().length);
+// console.log( balance.toFixed(3));
+
+// const otherNumber = 2123.5765
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++ Maths ++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(5.8));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(7.9));
+// console.log(Math.min(4,1,8,0,9));
+// console.log(Math.max(5,7,5.5,2,8));
+
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
+
+
+
