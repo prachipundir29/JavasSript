6635f12a3ea3d3ec89a81f448b5b57bde3d0ea86
prachipundir29 <prachipundir64@gmail.com>
2025-05-24 07:05:23 +0000

    Basics02

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+const myArr = [0, 1, 2, 3, 4, 5,6]
+// const fruit = [mango, apple, litchi, pear]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[0]);
+
+// myArr.push(8)
+// myArr.push(9)
+// myArr.pop(0)
+
+// myArr.unshift(4)
+// myArr.shift()
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join();
+
+// console.log(myArr);
+// console.log( typeof (newArr));
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c", myArr);
+console.log(myn2);
+
+
+
+
+
