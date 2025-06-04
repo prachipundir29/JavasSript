4a2911540a335f1450faac7a1bb72e5e1cfc7365
prachipundir29 <prachipundir64@gmail.com>
2025-06-04 06:07:02 +0000

    foreach

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -16,3 +16,29 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 // }
 // coding.forEach(printMe);
 
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+    
+// })
+
+const myLanguage = [
+    {
+      languageName : "JvavaScript",
+      languageFileName : "js"
+    },
+       {
+      languageName : "Java",
+      languageFileName : "java"
+    },
+       {
+      languageName : "python",
+      languageFileName : "py"
+    },
+]
+
+myLanguage.forEach((item) => {
+    console.log(item.languageName);
+    console.log(item.languageFileName);
+    
+})
+
