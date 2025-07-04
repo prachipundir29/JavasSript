15d7dda3bf95c75ee96601ed75ec2e9a8e4504cd
prachipundir29 <prachipundir64@gmail.com>
2025-07-04 11:35:36 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  
+// treat all code as ES6 version
+
+// alert(3+3); we are using nodejs, not browser
+
+let name = "Prachi";
+let age = 21;
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value/empty value  
+// undefined
+// symbol => unique
+
+// Object
+
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); // Object
+
+
---
+++ arrays.js
@@ -0,0 +1,6 @@
+console.log("Hello Class");
+console.log("Hello Yash");
+console.log("Hello Neha");
+console.log("Hello Rishi");
+console.log("Hello Kanak");
+console.log("Hello Smriti");
