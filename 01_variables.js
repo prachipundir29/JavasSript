bda0f64716ba8d1a5123da798468831db7c19488
prachipundir29 <prachipundir64@gmail.com>
2025-07-04 11:33:59 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 13325324;
+
+let accountEmail = "prachi@gmail.com";
+var accountPassword = "235364576565";
+ let accountCity = "Mumbai"
+
+accountEmail = "suhana@gamil.com"
+accountPassword =  "345865756"
+accountCity = "Delhi"
+
+
+/* prefer not to use var
+   beacuse of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
